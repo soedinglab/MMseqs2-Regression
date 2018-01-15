e25aca8114e3e5785a83dfc2b7bb5485a877e57a
Martin Steinegger <steinegger_martin@web.de>
2018-01-15 02:17:16 +0000

    switch linclust to set cover

--- scripts/run_codeship_pipeline.sh
+++ scripts/run_codeship_pipeline.sh
@@ -35,7 +35,7 @@ cp mmseqs-benchmark/data/clu.fasta mmseqs-benchmark/data/clu-tcov.fasta.gz small
 RUNEVAL="./mmseqs-benchmark/scripts/run_mmseqs_clu_regression.sh"
 time ${RUNEVAL} small-benchmark-db/clu.fasta ${MMSEQSAVX} CLU ${CI_COMMIT_ID} clu-results 0 "--min-seq-id 0.3 -s 2 --cluster-steps 3 --threads 16" \
     >> report-${CI_COMMIT_ID}
-time ${RUNEVAL} "small-benchmark-db/query.fasta small-benchmark-db/clu.fasta" ${MMSEQSAVX} LINCLU ${CI_COMMIT_ID} linclu-results 1 "--cov-mode 1 -c 0.90 --min-seq-id 0.50 --threads 16" \
+time ${RUNEVAL} "small-benchmark-db/query.fasta small-benchmark-db/clu.fasta" ${MMSEQSAVX} LINCLU ${CI_COMMIT_ID} linclu-results 1 "--cov-mode 1 --cluster-mode 0 -c 0.90 --min-seq-id 0.50 --threads 16" \
     >> report-${CI_COMMIT_ID}
 
 # fill out the report and fail
