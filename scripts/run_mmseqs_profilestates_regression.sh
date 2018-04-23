8406bb773541c0e3b9ff7c68d4c652675d66381a
Milot Mirdita <milot@mirdita.de>
2018-04-23 14:24:23 +0200

    fix codeship test

--- scripts/run_mmseqs_profilestates_regression.sh
+++ scripts/run_mmseqs_profilestates_regression.sh
@@ -22,19 +22,19 @@ function lap() { TIMERS+=($(date +%s.%N)); }
 rm -rf "$RESULTS"
 mkdir -p "$RESULTS/tmp"
 lap
-"${MMSEQS}" search "${TARGETDB}" "${TARGETDB}" "${RESULTS}/aln_target_profile" "${RESULTS}/tmp" -s 2 --e-profile 0.1 -e 0.1 -a --realign
+"${MMSEQS}" search "${TARGETDB}" "${TARGETDB}" "${RESULTS}/aln_target_profile" "${RESULTS}/tmp" -s 2 --e-profile 0.1 -e 0.1 -a --realign 1>&2 || exit 125
 lap
-"${MMSEQS}" result2profile "${TARGETDB}" "${TARGETDB}" "${RESULTS}/aln_target_profile" "${RESULTS}/target_profile"
+"${MMSEQS}" result2profile "${TARGETDB}" "${TARGETDB}" "${RESULTS}/aln_target_profile" "${RESULTS}/target_profile" 1>&2 || exit 125
 lap
-"${MMSEQS}" profile2cs "${RESULTS}/target_profile" "${RESULTS}/target_profile_states" 
+"${MMSEQS}" profile2cs "${RESULTS}/target_profile" "${RESULTS}/target_profile_states" 1>&2 || exit 125
 lap
-"${MMSEQS}" search "${QUERYDB}" "${TARGETDB}" "${RESULTS}/aln_query_target" "${RESULTS}/tmp" -s 2 --e-profile 0.1 -e 0.1 --realign -a
+"${MMSEQS}" search "${QUERYDB}" "${TARGETDB}" "${RESULTS}/aln_query_target" "${RESULTS}/tmp" -s 2 --e-profile 0.1 -e 0.1 --realign -a 1>&2 || exit 125
 lap
-"${MMSEQS}" result2profile "${QUERYDB}" "${TARGETDB}" "${RESULTS}/aln_query_target" "${RESULTS}/query_profile"
+"${MMSEQS}" result2profile "${QUERYDB}" "${TARGETDB}" "${RESULTS}/aln_query_target" "${RESULTS}/query_profile" 1>&2 || exit 125
 lap
-"${MMSEQS}" search "${RESULTS}/query_profile" "${RESULTS}/target_profile_states" "${RESULTS}/results_aln" "${RESULTS}/tmp" --max-seqs 4000 -e 100000.0 -s 2  --min-ungapped-score 0
+"${MMSEQS}" search "${RESULTS}/query_profile" "${RESULTS}/target_profile_states" "${RESULTS}/results_aln" "${RESULTS}/tmp" --max-seqs 4000 -e 100000.0 -s 2  --min-ungapped-score 0 1>&2 || exit 125
 lap
-"${MMSEQS}" convertalis "${QUERYDB}" "${TARGETDB}" "${RESULTS}/results_aln" "${RESULTS}/results_aln.m8"
+"${MMSEQS}" convertalis "${QUERYDB}" "${TARGETDB}" "${RESULTS}/results_aln" "${RESULTS}/results_aln.m8" 1>&2 || exit 125
 lap
 
 EVALPREFIX="${RESULTS}/evaluation"
