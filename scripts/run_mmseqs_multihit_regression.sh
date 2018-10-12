531e48a4382925f32c9aae32f7db00d6e9f84e2e
Milot Mirdita <milot@mirdita.de>
2018-10-12 14:59:55 +0200

    Fix results path in multihit reg

--- scripts/run_mmseqs_multihit_regression.sh
+++ scripts/run_mmseqs_multihit_regression.sh
@@ -9,20 +9,20 @@ THREADS="${6:-4}"
 
 BENCHDB="small-benchmark-db"
 
-mkdir -p "$RESULTDIR" && cd "$RESULTDIR"
+mkdir -p "$RESULTDIR"
 
 ##### multihitdb #####
-$MMSEQS multihitdb "$BENCHDIR/$BENCHDB/qset_01.fas.gz" "$BENCHDIR/$BENCHDB/qset_02.fas.gz" qsetsdb tmp 1>&2 || exit 125
-$MMSEQS multihitdb "$BENCHDIR/$BENCHDB/tset_01.fas.gz" "$BENCHDIR/$BENCHDB/tset_02.fas.gz" tsetsdb tmp 1>&2 || exit 125
+$MMSEQS multihitdb "$BENCHDIR/$BENCHDB/qset_01.fas.gz" "$BENCHDIR/$BENCHDB/qset_02.fas.gz" "$RESULTDIR/qsetsdb" tmp 1>&2 || exit 125
+$MMSEQS multihitdb "$BENCHDIR/$BENCHDB/tset_01.fas.gz" "$BENCHDIR/$BENCHDB/tset_02.fas.gz" "$RESULTDIR/tsetsdb" tmp 1>&2 || exit 125
 
 ##### multihitsearch #####
-$MMSEQS multihitsearch qsetsdb tsetsdb result tmp --threads ${THREADS} 1>&2 || exit 125
+$MMSEQS multihitsearch "$RESULTDIR/qsetsdb" "$RESULTDIR/tsetsdb" "$RESULTDIR/result" "$RESULTDIR/tmp" --threads ${THREADS} 1>&2 || exit 125
 
-##### combinepvalperset #####
-$MMSEQS combinepvalperset qsetsdb tsetsdb result pval tmp --threads ${THREADS} 1>&2 || exit 125
+##### combine"$RESULTDIR/pval"perset #####
+$MMSEQS combinepvalperset "$RESULTDIR/qsetsdb" "$RESULTDIR/tsetsdb" "$RESULTDIR/result" "$RESULTDIR/pval" "$RESULTDIR/tmp" --threads ${THREADS} 1>&2 || exit 125
 
-EVAL1="$(tr -d '\000' < pval | head -n1 | cut -f2)"
-EVAL2="$(tr -d '\000' < pval | tail -n1 | cut -f2)"
+EVAL1="$(tr -d '\000' < "$RESULTDIR/pval" | head -n1 | cut -f2)"
+EVAL2="$(tr -d '\000' < "$RESULTDIR/pval" | tail -n1 | cut -f2)"
 
 echo -e "${NAME}\t${VERSION}\t12\t${EVAL1}"
 echo -e "${NAME}\t${VERSION}\t13\t${EVAL2}"
