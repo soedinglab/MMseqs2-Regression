113879dbaeb903605a846a475e6a10c74c5b1229
Martin Steinegger <martin.steinegger@mpibpc.mpg.de>
2018-11-02 11:57:20 -0400

    Add nucl/nucl workflow

--- scripts/run_codeship_pipeline.sh
+++ scripts/run_codeship_pipeline.sh
@@ -60,5 +60,5 @@ time ${RUNEVAL} "./mmseqs-benchmark" ${MMSEQSAVX} "./mmseqs-benchmark/scripts" $
 # fill out the report and fail
 cat report-${CI_COMMIT_ID}
 #curl -F upfile=@report-${CI_COMMIT_ID} https://mmseqs.com/regression.php?secret=${REGRESSIONSECRET}
-./mmseqs-benchmark/scripts/regression_report.sh report-${CI_COMMIT_ID} 0.235 0.334 0.235 0.142 0.140 0.245 17299 26823 8.387E-203 3.613E-142 0
+./mmseqs-benchmark/scripts/regression_report.sh report-${CI_COMMIT_ID} 0.235 0.334 0.235 0.177 0.142 0.140 0.245 17299 26823 8.387E-203 3.613E-142 0
 exit $?
