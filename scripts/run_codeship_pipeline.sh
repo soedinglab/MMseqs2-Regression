541146738ba722a138188b48ba6ec34ab197f5ef
Milot <milot@mirdita.de>
2019-01-11 10:14:18 +0000

    Enable multihit regression again

--- scripts/run_codeship_pipeline.sh
+++ scripts/run_codeship_pipeline.sh
@@ -58,8 +58,8 @@ time ${RUNEVAL} small-benchmark-db/clu.fasta ${MMSEQSAVX} CLU ${CI_COMMIT_ID} cl
 time ${RUNEVAL} "small-benchmark-db/clu.fasta" ${MMSEQSAVX} LINCLU ${CI_COMMIT_ID} linclu-results 1 "--cov-mode 1 --cluster-mode 0 -c 0.90 --min-seq-id 0.50 --threads 16" \
     >> report-${CI_COMMIT_ID}
 
-#RUNEVAL="./mmseqs-benchmark/scripts/run_mmseqs_multihit_regression.sh"
-#time ${RUNEVAL} . ${MMSEQSAVX} ${CI_COMMIT_ID} multihit-results MULTHIT 16 >> report-${CI_COMMIT_ID}
+RUNEVAL="./mmseqs-benchmark/scripts/run_mmseqs_multihit_regression.sh"
+time ${RUNEVAL} . ${MMSEQSAVX} ${CI_COMMIT_ID} multihit-results MULTHIT 16 >> report-${CI_COMMIT_ID}
 
 RUNEVAL="./mmseqs-benchmark/scripts/run_mmseqs_extractorfs.sh"
 time ${RUNEVAL} "./mmseqs-benchmark" ${MMSEQSAVX} "./mmseqs-benchmark/scripts" ${CI_COMMIT_ID} results EXTRACTORFS 16 >> report-${CI_COMMIT_ID}
@@ -71,5 +71,5 @@ time ${RUNEVAL} . ${MMSEQSAVX} ${EVALUATE} ${CI_COMMIT_ID} results NUCLNUCL_SEAR
 # fill out the report and fail
 cat report-${CI_COMMIT_ID}
 #curl -F upfile=@report-${CI_COMMIT_ID} https://mmseqs.com/regression.php?secret=${REGRESSIONSECRET}
-./mmseqs-benchmark/scripts/regression_report.sh report-${CI_COMMIT_ID} 0.235 0.334 0.235 0.142 0.140 0.245 17272 26523 1.112E-202 4.032E-142 0 0.177 
+./mmseqs-benchmark/scripts/regression_report.sh report-${CI_COMMIT_ID} 0.235 0.334 0.235 0.142 0.140 0.245 17272 26523 0 0 0 0.177 
 exit $?
