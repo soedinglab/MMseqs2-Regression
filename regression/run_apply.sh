f57c98015c639b493d1a900d276404e3228096b0
Milot Mirdita <milot@mirdita.de>
2019-05-29 11:11:58 +0200

    Fix invalid path in apply regression

--- regression/run_apply.sh
+++ regression/run_apply.sh
@@ -7,7 +7,7 @@ mkdir -p "${RESULTS}"
 "${MMSEQS}" createdb "${DATADIR}/query.fasta" "${RESULTS}/query"
 "${MMSEQS}" apply "${RESULTS}/query" "$RESULTS/apply" -- wc -c
 
-ACTUAL="$(tr -d '\000' < /Users/mirdita/repositories/mmseqs-benchmark/APPLY/apply | awk '{ l += $1; } END { print l }')"
+ACTUAL="$(tr -d '\000' < "$RESULTS/apply" | awk '{ l += $1; } END { print l }')"
 TARGET="$(grep -v "^>" "${DATADIR}/query.fasta" | wc -c)"
 awk -v actual="$ACTUAL" -v target="$TARGET" \
     'BEGIN { print (actual == target) ? "GOOD" : "BAD"; print "Expected: ", target; print "Actual: ", actual; }' \
