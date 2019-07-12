6c49c7c018ef8b597558ae6f051a5db48b839d86
Milot Mirdita <milot@mirdita.de>
2019-07-12 14:20:30 +0200

    Rewrite tests to not cd to subdirectory (can break relative paths)

--- regression/run_slicetechnical.sh
+++ regression/run_slicetechnical.sh
@@ -4,16 +4,14 @@ DATADIR="${2}"
 RESULTS="${3}"
 
 mkdir -p "${RESULTS}"
-cd "${RESULTS}"
-
 # create profiles db from fasta #
-"${MMSEQS}" createdb "${DATADIR}/five_profiles.fasta" prof_5
-"${MMSEQS}" mergedbs prof_5 prof_5_fasta prof_5_h prof_5 --prefixes ">"
-awk 'BEGIN { printf("%c%c%c%c",11,0,0,0); exit; }' > prof_5_fasta.dbtype
-"${MMSEQS}" msa2profile prof_5_fasta five_profiles --filter-msa 0
+"${MMSEQS}" createdb "${DATADIR}/five_profiles.fasta" "${RESULTS}/prof_5"
+"${MMSEQS}" mergedbs "${RESULTS}/prof_5" "${RESULTS}/prof_5_fasta" "${RESULTS}/prof_5_h" "${RESULTS}/prof_5" --prefixes ">"
+awk 'BEGIN { printf("%c%c%c%c",11,0,0,0); exit; }' > "${RESULTS}/prof_5_fasta.dbtype"
+"${MMSEQS}" msa2profile "${RESULTS}/prof_5_fasta" "${RESULTS}/five_profiles" --filter-msa 0
 
 # create db from duplicated query #
-"${MMSEQS}" createdb "${DATADIR}/query_pow2.fasta" query_pow2
+"${MMSEQS}" createdb "${DATADIR}/query_pow2.fasta" "${RESULTS}/query_pow2"
 
 PROFILES_DB="five_profiles"
 PROTEINS_DB="query_pow2"
@@ -21,23 +19,19 @@ PROTEINS_DB="query_pow2"
 FINAL_COUNTS_AS_SHOULD="512,256,128,64,32"
 COUNTS_ALL_TESTS="${FINAL_COUNTS_AS_SHOULD}"
 
-for SPLIT_MODE in 0 1
-do
+for SPLIT_MODE in 0 1; do
 	DISK_SPACE_LIMIT_KB=17
-	OUTPUT_SUBDIR="DSL_${DISK_SPACE_LIMIT_KB}K_SPLIT_MODE_${SPLIT_MODE}"
-	echo "working on: ${OUTPUT_SUBDIR}"
-	mkdir -p "${OUTPUT_SUBDIR}"
-	cd "${OUTPUT_SUBDIR}"
+	SPLIT_OUTPUT="${RESULTS}/DSL_${DISK_SPACE_LIMIT_KB}K_SPLIT_MODE_${SPLIT_MODE}"
+	mkdir -p "${SPLIT_OUTPUT}"
 	# run slice search #
-	"${MMSEQS}" search ../"${PROTEINS_DB}" ../"${PROFILES_DB}" res tmpFolder --slice-search -s 1 --disk-space-limit "${DISK_SPACE_LIMIT_KB}K" --split-mode "${SPLIT_MODE}"
-	"${MMSEQS}" convertalis ../"${PROTEINS_DB}" ../"${PROFILES_DB}" res alis
-	awk '{print $1}' alis | sort | uniq -c | sort -nr | awk '{ print $1 }' | paste -d, -s - > final_counts.txt
-	COUNTS_CURR_TEST="$(cat < final_counts.txt)"
+	"${MMSEQS}" search "${RESULTS}/query_pow2" "${RESULTS}/five_profiles" "${SPLIT_OUTPUT}/res" "${SPLIT_OUTPUT}/tmpFolder" --slice-search -s 1 --disk-space-limit "${DISK_SPACE_LIMIT_KB}K" --split-mode "${SPLIT_MODE}"
+	"${MMSEQS}" convertalis "${RESULTS}/query_pow2" "${RESULTS}/five_profiles" "${SPLIT_OUTPUT}/res" "${SPLIT_OUTPUT}/alis"
+	awk '{print $1}' "${SPLIT_OUTPUT}/alis" | sort | uniq -c | sort -nr | awk '{ print $1 }' | paste -d, -s - > "${SPLIT_OUTPUT}/final_counts.txt"
+	COUNTS_CURR_TEST="$(cat < "${SPLIT_OUTPUT}/final_counts.txt")"
 	echo "$COUNTS_CURR_TEST"
 	if [ "$COUNTS_CURR_TEST" != "$FINAL_COUNTS_AS_SHOULD" ]; then
 		COUNTS_ALL_TESTS="$COUNTS_CURR_TEST"
 	fi
-	cd "${RESULTS}"
 done
 
 # if ALL tests passed - GOOD, otherwise - BAD
--- regression/run_split.sh
+++ regression/run_split.sh
@@ -4,31 +4,27 @@ DATADIR="${2}"
 RESULTS="${3}"
 SPLIT_MODE="${4}"
 
-mkdir -p "${RESULTS}"
-cd "${RESULTS}"
-head -n 1000 "${DATADIR}/query.fasta" > "500prots.fasta"
-"${MMSEQS}" createdb "500prots.fasta" prots --dbtype 0
-
 NUM_RES_LINES_BASELINE=500
 
+mkdir -p "${RESULTS}"
+head -n $((NUM_RES_LINES_BASELINE*2)) "${DATADIR}/query.fasta" > "${RESULTS}/500prots.fasta"
+"${MMSEQS}" createdb "${RESULTS}/500prots.fasta" "${RESULTS}/prots" --dbtype 0
+
 NUM_LINES_ALL_TESTS="${NUM_RES_LINES_BASELINE}"
-for NUM_SPLITS in 1 5
-do
-	# target split:
-	OUTPUT_SUBDIR="NS_${NUM_SPLITS}"
-	echo "working on: ${OUTPUT_SUBDIR}"
-	mkdir -p "${OUTPUT_SUBDIR}"
-	cd "${OUTPUT_SUBDIR}"
-	"${MMSEQS}" search ../prots ../prots res tmpFolder -s 1 --split-mode "${SPLIT_MODE}" --split "${NUM_SPLITS}"
-	"${MMSEQS}" convertalis ../prots ../prots res res.m8
-	NUM_LINES_CURR_TEST="$(wc -l < res.m8)"
+for NUM_SPLITS in 1 5; do
+	# target split
+	SPLIT_OUTPUT="${RESULTS}/NS_${NUM_SPLITS}"
+	mkdir -p "${SPLIT_OUTPUT}"
+	"${MMSEQS}" search "${RESULTS}/prots" "${RESULTS}/prots" "${SPLIT_OUTPUT}/res" "${SPLIT_OUTPUT}/tmpFolder" -s 1 --split-mode "${SPLIT_MODE}" --split "${NUM_SPLITS}"
+	"${MMSEQS}" convertalis "${RESULTS}/prots" "${RESULTS}/prots" "${SPLIT_OUTPUT}/res" "${SPLIT_OUTPUT}/res.m8"
+	NUM_LINES_CURR_TEST="$(wc -l < "${SPLIT_OUTPUT}/res.m8")"
 	if [ "${NUM_LINES_CURR_TEST}" -ne "${NUM_RES_LINES_BASELINE}" ]; then
 		NUM_LINES_ALL_TESTS="${NUM_LINES_CURR_TEST}"
 	fi
-	cd "${RESULTS}"
 done
 
 # if ALL tests passed - GOOD, otherwise - BAD
 awk -v actual="$NUM_LINES_ALL_TESTS" -v target="$NUM_RES_LINES_BASELINE" \
 	'BEGIN { print (actual == target) ? "GOOD" : "BAD"; print "Expected: ", target; print "Actual: ", actual; }' \
 	> "${RESULTS}/report"
+
