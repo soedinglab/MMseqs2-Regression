6c876d9aed816fda7ee7799ebea82c335428ad8b
Martin Steinegger <themartinsteinegger@gmail.com>
2019-08-05 01:48:36 -0400

    Add regression for cluster reassign

---
+++ regression/run_easy_cluster_reassign.sh
@@ -0,0 +1,14 @@
+#!/bin/sh -e
+MMSEQS="${1}"
+DATADIR="${2}"
+RESULTS="${3}"
+mkdir -p "${RESULTS}"
+
+"${MMSEQS}" easy-cluster "${DATADIR}/clu.fasta" "$RESULTS/results" "$RESULTS/tmp" --min-seq-id 0.3 -s 2 -c 0.8 --cov-mode 1 --cluster-reassign 1
+
+awk 'BEGIN { l = "" } l != $1 { l = $1; cnt++; } { t++; } END { print cnt"\t"t"\t"(t/cnt) }' "$RESULTS/results_cluster.tsv" > "$RESULTS/results_summary.tsv"
+ACTUAL="$(cut -f1 "$RESULTS/results_summary.tsv")"
+TARGET="17290"
+awk -v actual="$ACTUAL" -v target="$TARGET" \
+    'BEGIN { print (actual == target) ? "GOOD" : "BAD"; print "Expected: ", target; print "Actual: ", actual; }' \
+    > "${RESULTS}/report"
