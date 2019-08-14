27f020921bc58f4cad331ce473513d3eb8d3d4cd
Martin Steinegger <themartinsteinegger@gmail.com>
2019-08-14 18:06:47 -0400

    Fix reassing regression value

--- regression/run_easy_cluster_reassign.sh
+++ regression/run_easy_cluster_reassign.sh
@@ -8,7 +8,7 @@ mkdir -p "${RESULTS}"
 
 awk 'BEGIN { l = "" } l != $1 { l = $1; cnt++; } { t++; } END { print cnt"\t"t"\t"(t/cnt) }' "$RESULTS/results_cluster.tsv" > "$RESULTS/results_summary.tsv"
 ACTUAL="$(cut -f1 "$RESULTS/results_summary.tsv")"
-TARGET="17290"
+TARGET="17232"
 awk -v actual="$ACTUAL" -v target="$TARGET" \
     'BEGIN { print (actual == target) ? "GOOD" : "BAD"; print "Expected: ", target; print "Actual: ", actual; }' \
     > "${RESULTS}/report"
