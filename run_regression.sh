a2fad4f0bbac6cd2c43dbe8fb0d87bbb287bbca0
Milot Mirdita <milot@mirdita.de>
2019-08-15 11:11:08 +0200

    Refactor test runner for prettier output with time

--- run_regression.sh
+++ run_regression.sh
@@ -1,9 +1,24 @@
 #!/bin/sh -e
-MMSEQS="${1}"
-RESULTS="$( (readlink -f -- "$2" 2>/dev/null) || (greadlink -f -- "$2") )"
+abspath() {
+    if [ -d "$1" ]; then
+        (cd "$1"; pwd)
+    elif [ -f "$1" ]; then
+        if [ -z "${1##*/*}" ]; then
+            echo "$(cd "${1%/*}"; pwd)/${1##*/}"
+        else
+            echo "$(pwd)/$1"
+        fi
+    elif [ -d "$(dirname "$1")" ]; then
+        echo "$(cd "$(dirname "$1")"; pwd)/$(basename "$1")"
+    fi
+}
+
+MMSEQS="$(abspath "$1")"
+RESULTS="$(abspath "$2")"
+
 mkdir -p "${RESULTS}"
 
-BASE="$(dirname "$( (readlink -f -- "$0" 2>/dev/null) || (greadlink -f -- "$0") )")"
+BASE="$(dirname "$(abspath "$0")")"
 cd "${BASE}"
 
 # build the benchmark tools
@@ -15,83 +30,96 @@ EVALUATE="${BASE}/build/evaluate_results"
 SAMTOOLS="${BASE}/samtools/samtools.sh"
 
 TESTS=""
-# continue on if one test fails
+run_test() {
+	NAME="$1"
+	TESTS="${TESTS} ${NAME}"
+	shift
+	START="$(date +%s)"
+	"$@"
+	END="$(date +%s)"
+    eval "${NAME}_TIME"="$((END-START))"
+}
+
+# continue on if one test fail
 set +e
-time "${SCRIPTS}/run_search.sh" "${MMSEQS}" "${EVALUATE}" "${DATADIR}" "${RESULTS}/SEARCH"; TESTS="SEARCH ${TESTS}"
-time "${SCRIPTS}/run_easy_search.sh" "${MMSEQS}" "${EVALUATE}" "${DATADIR}" "${RESULTS}/EASY_SEARCH"; TESTS="EASY_SEARCH ${TESTS}"
-time "${SCRIPTS}/run_profile.sh" "${MMSEQS}" "${EVALUATE}" "${DATADIR}" "${RESULTS}/PROFILE"; TESTS="PROFILE ${TESTS}"
-time "${SCRIPTS}/run_easy_profile.sh" "${MMSEQS}" "${EVALUATE}" "${DATADIR}" "${RESULTS}/EASY_PROFILE"; TESTS="EASY_PROFILE ${TESTS}"
-time "${SCRIPTS}/run_sliceprofile.sh" "${MMSEQS}" "${EVALUATE}" "${DATADIR}" "${RESULTS}/SLICEPROFILE"; TESTS="SLICEPROFILE ${TESTS}"
-time "${SCRIPTS}/run_dbprofile.sh" "${MMSEQS}" "${EVALUATE}" "${DATADIR}" "${RESULTS}/DBPROFILE"; TESTS="DBPROFILE ${TESTS}"
-time "${SCRIPTS}/run_nuclprot.sh" "${MMSEQS}" "${EVALUATE}" "${DATADIR}" "${RESULTS}/NUCLPROT_SEARCH"; TESTS="NUCLPROT_SEARCH ${TESTS}"
-time "${SCRIPTS}/run_nuclnucl.sh" "${MMSEQS}" "${SAMTOOLS}" "${EVALUATE}" "${DATADIR}" "${RESULTS}/NUCLNUCL_SEARCH"; TESTS="NUCLNUCL_SEARCH ${TESTS}"
-time "${SCRIPTS}/run_nuclnucl_translated.sh" "${MMSEQS}" "${SAMTOOLS}" "${EVALUATE}" "${DATADIR}" "${RESULTS}/NUCLNUCL_TRANS_SEARCH"; TESTS="NUCLNUCL_TRANS_SEARCH ${TESTS}"
-time "${SCRIPTS}/run_cluster.sh" "${MMSEQS}" "${DATADIR}" "${RESULTS}/CLUSTER"; TESTS="CLUSTER ${TESTS}"
-time "${SCRIPTS}/run_easy_cluster.sh" "${MMSEQS}" "${DATADIR}" "${RESULTS}/EASY_CLUSTER"; TESTS="EASY_CLUSTER ${TESTS}"
-time "${SCRIPTS}/run_easy_cluster_reassign.sh" "${MMSEQS}" "${DATADIR}" "${RESULTS}/CLUSTER_REASSIGN"; TESTS="CLUSTER_REASSIGN ${TESTS}"
-time "${SCRIPTS}/run_linclust.sh" "${MMSEQS}" "${DATADIR}" "${RESULTS}/LINCLUST"; TESTS="LINCLUST ${TESTS}"
-time "${SCRIPTS}/run_linclust_split.sh" "${MMSEQS}" "${DATADIR}" "${RESULTS}/LINCLUST_SPLIT"; TESTS="LINCLUST_SPLIT ${TESTS}"
-time "${SCRIPTS}/run_easy_linclust.sh" "${MMSEQS}" "${DATADIR}" "${RESULTS}/EASY_LINCLUST"; TESTS="EASY_LINCLUST ${TESTS}"
-time "${SCRIPTS}/run_protnucl.sh" "${MMSEQS}" "${EVALUATE}" "${DATADIR}" "${RESULTS}/PROTNUCL_SEARCH"; TESTS="PROTNUCL_SEARCH ${TESTS}"
-time "${SCRIPTS}/run_nuclprottax.sh" "${MMSEQS}" "${EVALUATE}" "${DATADIR}" "${RESULTS}/NUCLPROTTAX_SEARCH"; TESTS="NUCLPROTTAX_SEARCH ${TESTS}"
-time "${SCRIPTS}/run_dbprofile_index.sh" "${MMSEQS}" "${EVALUATE}" "${DATADIR}" "${RESULTS}/DBPROFILE_INDEX"; TESTS="DBPROFILE_INDEX ${TESTS}"
-time "${SCRIPTS}/run_nuclnucl_linsearchtranslated.sh" "${MMSEQS}" "${SAMTOOLS}" "${EVALUATE}" "${DATADIR}" "${RESULTS}/LINSEARCH_NUCLNUCL_TARNS_SEARCH"; TESTS="LINSEARCH_NUCLNUCL_TARNS_SEARCH ${TESTS}"
-time "${SCRIPTS}/run_nuclnucl_linsearch.sh" "${MMSEQS}" "${SAMTOOLS}" "${EVALUATE}" "${DATADIR}" "${RESULTS}/LINSEARCH_NUCLNUCL_SEARCH"; TESTS="LINSEARCH_NUCLNUCL_SEARCH ${TESTS}"
-time "${SCRIPTS}/run_easy_nuclnucl_linsearch_split.sh" "${MMSEQS}" "${SAMTOOLS}" "${EVALUATE}" "${DATADIR}" "${RESULTS}/EASY_LINSEARCH_NUCLNUCL_SEARCH_SPLIT"; TESTS="/EASY_LINSEARCH_NUCLNUCL_SEARCH_SPLIT ${TESTS}"
-time "${SCRIPTS}/run_cluster_update.sh" "${MMSEQS}" "${DATADIR}" "${RESULTS}/LINCLUST_UPDATE"; TESTS="LINCLUST_UPDATE ${TESTS}"
-# time "${SCRIPTS}/run_multihit.sh" "${MMSEQS}" "${DATADIR}" "${RESULTS}/MULTHIT"; TESTS="MULTHIT ${TESTS}"
-time "${SCRIPTS}/run_easynuclnucltax.sh" "${MMSEQS}" "${EVALUATE}" "${DATADIR}" "${RESULTS}/EASYNUCLNUCLTAX_SEARCH"; TESTS="EASYNUCLNUCLTAX_SEARCH ${TESTS}"
-time "${SCRIPTS}/run_extractorfs.sh" "${MMSEQS}" "${SCRIPTS}/extractorfs.pl" "${SCRIPTS}/compare_frags.pl" "${DATADIR}" "${RESULTS}/EXTRACTORFS"; TESTS="EXTRACTORFS ${TESTS}"
-time "${SCRIPTS}/run_rbh.sh" "${MMSEQS}" "${DATADIR}" "${RESULTS}/RBH"; TESTS="RBH ${TESTS}"
-time "${SCRIPTS}/run_apply.sh" "${MMSEQS}" "${DATADIR}" "${RESULTS}/APPLY"; TESTS="APPLY ${TESTS}"
-# time "${SCRIPTS}/run_profilestates.sh" "${MMSEQS}" "${EVALUATE}" "${DATADIR}" "${RESULTS}/CSPROFILE"; TESTS="CSPROFILE ${TESTS}"
-time "${SCRIPTS}/run_index_compatible.sh" "${MMSEQS}" "${DATADIR}" "${RESULTS}/INDEX_COMPATIBLE"; TESTS="INDEX_COMPATIBLE ${TESTS}"
+
+run_test SEARCH "${SCRIPTS}/run_search.sh" "${MMSEQS}" "${EVALUATE}" "${DATADIR}" "${RESULTS}/SEARCH"
+run_test EASY_SEARCH "${SCRIPTS}/run_easy_search.sh" "${MMSEQS}" "${EVALUATE}" "${DATADIR}" "${RESULTS}/EASY_SEARCH"
+run_test PROFILE "${SCRIPTS}/run_profile.sh" "${MMSEQS}" "${EVALUATE}" "${DATADIR}" "${RESULTS}/PROFILE"
+run_test EASY_PROFILE "${SCRIPTS}/run_easy_profile.sh" "${MMSEQS}" "${EVALUATE}" "${DATADIR}" "${RESULTS}/EASY_PROFILE"
+run_test SLICEPROFILE "${SCRIPTS}/run_sliceprofile.sh" "${MMSEQS}" "${EVALUATE}" "${DATADIR}" "${RESULTS}/SLICEPROFILE"
+run_test DBPROFILE "${SCRIPTS}/run_dbprofile.sh" "${MMSEQS}" "${EVALUATE}" "${DATADIR}" "${RESULTS}/DBPROFILE"
+run_test NUCLPROT_SEARCH "${SCRIPTS}/run_nuclprot.sh" "${MMSEQS}" "${EVALUATE}" "${DATADIR}" "${RESULTS}/NUCLPROT_SEARCH"
+run_test NUCLNUCL_SEARCH "${SCRIPTS}/run_nuclnucl.sh" "${MMSEQS}" "${SAMTOOLS}" "${EVALUATE}" "${DATADIR}" "${RESULTS}/NUCLNUCL_SEARCH"
+run_test NUCLNUCL_TRANS_SEARCH "${SCRIPTS}/run_nuclnucl_translated.sh" "${MMSEQS}" "${SAMTOOLS}" "${EVALUATE}" "${DATADIR}" "${RESULTS}/NUCLNUCL_TRANS_SEARCH"
+run_test CLUSTER "${SCRIPTS}/run_cluster.sh" "${MMSEQS}" "${DATADIR}" "${RESULTS}/CLUSTER"
+run_test EASY_CLUSTER "${SCRIPTS}/run_easy_cluster.sh" "${MMSEQS}" "${DATADIR}" "${RESULTS}/EASY_CLUSTER"
+run_test CLUSTER_REASSIGN "${SCRIPTS}/run_easy_cluster_reassign.sh" "${MMSEQS}" "${DATADIR}" "${RESULTS}/CLUSTER_REASSIGN"
+run_test LINCLUST "${SCRIPTS}/run_linclust.sh" "${MMSEQS}" "${DATADIR}" "${RESULTS}/LINCLUST"
+run_test LINCLUST_SPLIT "${SCRIPTS}/run_linclust_split.sh" "${MMSEQS}" "${DATADIR}" "${RESULTS}/LINCLUST_SPLIT"
+run_test EASY_LINCLUST "${SCRIPTS}/run_easy_linclust.sh" "${MMSEQS}" "${DATADIR}" "${RESULTS}/EASY_LINCLUST"
+run_test PROTNUCL_SEARCH "${SCRIPTS}/run_protnucl.sh" "${MMSEQS}" "${EVALUATE}" "${DATADIR}" "${RESULTS}/PROTNUCL_SEARCH"
+run_test NUCLPROTTAX_SEARCH "${SCRIPTS}/run_nuclprottax.sh" "${MMSEQS}" "${EVALUATE}" "${DATADIR}" "${RESULTS}/NUCLPROTTAX_SEARCH"
+run_test DBPROFILE_INDEX "${SCRIPTS}/run_dbprofile_index.sh" "${MMSEQS}" "${EVALUATE}" "${DATADIR}" "${RESULTS}/DBPROFILE_INDEX"
+run_test LINSEARCH_NUCLNUCL_TARNS_SEARCH "${SCRIPTS}/run_nuclnucl_linsearchtranslated.sh" "${MMSEQS}" "${SAMTOOLS}" "${EVALUATE}" "${DATADIR}" "${RESULTS}/LINSEARCH_NUCLNUCL_TARNS_SEARCH"
+run_test LINSEARCH_NUCLNUCL_SEARCH "${SCRIPTS}/run_nuclnucl_linsearch.sh" "${MMSEQS}" "${SAMTOOLS}" "${EVALUATE}" "${DATADIR}" "${RESULTS}/LINSEARCH_NUCLNUCL_SEARCH"
+run_test EASY_LINSEARCH_NUCLNUCL_SEARCH_SPLIT "${SCRIPTS}/run_easy_nuclnucl_linsearch_split.sh" "${MMSEQS}" "${SAMTOOLS}" "${EVALUATE}" "${DATADIR}" "${RESULTS}/EASY_LINSEARCH_NUCLNUCL_SEARCH_SPLIT"
+run_test LINCLUST_UPDATE "${SCRIPTS}/run_cluster_update.sh" "${MMSEQS}" "${DATADIR}" "${RESULTS}/LINCLUST_UPDATE"
+run_test EASYNUCLNUCLTAX_SEARCH "${SCRIPTS}/run_easynuclnucltax.sh" "${MMSEQS}" "${EVALUATE}" "${DATADIR}" "${RESULTS}/EASYNUCLNUCLTAX_SEARCH"
+run_test EXTRACTORFS "${SCRIPTS}/run_extractorfs.sh" "${MMSEQS}" "${SCRIPTS}/extractorfs.pl" "${SCRIPTS}/compare_frags.pl" "${DATADIR}" "${RESULTS}/EXTRACTORFS"
+run_test RBH "${SCRIPTS}/run_rbh.sh" "${MMSEQS}" "${DATADIR}" "${RESULTS}/RBH"
+run_test APPLY "${SCRIPTS}/run_apply.sh" "${MMSEQS}" "${DATADIR}" "${RESULTS}/APPLY"
+run_test INDEX_COMPATIBLE "${SCRIPTS}/run_index_compatible.sh" "${MMSEQS}" "${DATADIR}" "${RESULTS}/INDEX_COMPATIBLE"
+# run_test MULTHIT "${SCRIPTS}/run_multihit.sh" "${MMSEQS}" "${DATADIR}" "${RESULTS}/MULTHIT"
+# run_test CSPROFILE "${SCRIPTS}/run_profilestates.sh" "${MMSEQS}" "${EVALUATE}" "${DATADIR}" "${RESULTS}/CSPROFILE"
 
 case "$("${MMSEQS}" version)" in
 	*MPI)
 		export RUNNER="mpirun -np 1"
-		time "${SCRIPTS}/run_split.sh" "${MMSEQS}" "${DATADIR}" "${RESULTS}/MPI_TARGET_SPLIT_NP1" 0; TESTS="MPI_TARGET_SPLIT_NP1 ${TESTS}"
-		time "${SCRIPTS}/run_split.sh" "${MMSEQS}" "${DATADIR}" "${RESULTS}/MPI_QUERY_SPLIT_NP1" 1; TESTS="MPI_QUERY_SPLIT_NP1 ${TESTS}"
-		time "${SCRIPTS}/run_slicetechnical.sh" "${MMSEQS}" "${DATADIR}" "${RESULTS}/MPI_SLICE_TECH_NP1"; TESTS="MPI_SLICE_TECH_NP1 ${TESTS}"
+		run_test MPI_TARGET_SPLIT_NP1 "${SCRIPTS}/run_split.sh" "${MMSEQS}" "${DATADIR}" "${RESULTS}/MPI_TARGET_SPLIT_NP1" 0
+		run_test MPI_QUERY_SPLIT_NP1 "${SCRIPTS}/run_split.sh" "${MMSEQS}" "${DATADIR}" "${RESULTS}/MPI_QUERY_SPLIT_NP1" 1
+		run_test MPI_SLICE_TECH_NP1 "${SCRIPTS}/run_slicetechnical.sh" "${MMSEQS}" "${DATADIR}" "${RESULTS}/MPI_SLICE_TECH_NP1"
 		
 		export RUNNER="mpirun -np 3"
-		time "${SCRIPTS}/run_split.sh" "${MMSEQS}" "${DATADIR}" "${RESULTS}/MPI_TARGET_SPLIT_NP3" 0; TESTS="MPI_TARGET_SPLIT_NP3 ${TESTS}"
-		time "${SCRIPTS}/run_split.sh" "${MMSEQS}" "${DATADIR}" "${RESULTS}/MPI_QUERY_SPLIT_NP3" 1; TESTS="MPI_QUERY_SPLIT_NP3 ${TESTS}"
-		time "${SCRIPTS}/run_slicetechnical.sh" "${MMSEQS}" "${DATADIR}" "${RESULTS}/MPI_SLICE_TECH_NP3"; TESTS="MPI_SLICE_TECH_NP3 ${TESTS}"
+		run_test MPI_TARGET_SPLIT_NP3 "${SCRIPTS}/run_split.sh" "${MMSEQS}" "${DATADIR}" "${RESULTS}/MPI_TARGET_SPLIT_NP3" 0
+		run_test MPI_QUERY_SPLIT_NP3 "${SCRIPTS}/run_split.sh" "${MMSEQS}" "${DATADIR}" "${RESULTS}/MPI_QUERY_SPLIT_NP3" 1
+		run_test MPI_SLICE_TECH_NP3 "${SCRIPTS}/run_slicetechnical.sh" "${MMSEQS}" "${DATADIR}" "${RESULTS}/MPI_SLICE_TECH_NP3"
 		
 		unset RUNNER
 		;;
 	*)
-		time "${SCRIPTS}/run_split.sh" "${MMSEQS}" "${DATADIR}" "${RESULTS}/NOMPI_TARGET_SPLIT" 0; TESTS="NOMPI_TARGET_SPLIT ${TESTS}"
-		time "${SCRIPTS}/run_slicetechnical.sh" "${MMSEQS}" "${DATADIR}" "${RESULTS}/NOMPI_SLICE_TECH"; TESTS="NOMPI_SLICE_TECH ${TESTS}"
+		run_test NOMPI_TARGET_SPLIT "${SCRIPTS}/run_split.sh" "${MMSEQS}" "${DATADIR}" "${RESULTS}/NOMPI_TARGET_SPLIT" 0
+		run_test NOMPI_SLICE_TECH "${SCRIPTS}/run_slicetechnical.sh" "${MMSEQS}" "${DATADIR}" "${RESULTS}/NOMPI_SLICE_TECH"
 esac
 
 set -e
-
 printf "\n"
 ERR=0
 for i in ${TESTS} ; do
-    echo "$i"
+    VAL="${i}_TIME"
+    eval TIME="\$$VAL"
+    printf "\033[1m$i (Time: %ss)\033[0m\n" "${TIME}"
     if [ ! -f "${RESULTS}/${i}/report" ]; then
-        printf "TEST FAILED (NO REPORT)\n\n"
+        printf "\033[33mTEST FAILED (NO REPORT)\033[0m\n\n"
         ERR=$((ERR+1))
         continue
     fi
 
     if [ ! -s "${RESULTS}/${i}/report" ]; then
-        printf "TEST FAILED (EMPTY REPORT)\n\n"
+        printf "\033[33mTEST FAILED (EMPTY REPORT)\033[0m\n\n"
         ERR=$((ERR+1))
         continue
     fi
     STATUS="$(head -n 1 "${RESULTS}/${i}/report")"
     if [ "$STATUS" != "GOOD" ]; then
-        echo "TEST FAILED"
+        printf "\033[31mTEST FAILED\033[0m\n"
         ERR=$((ERR+1))
     else
-        echo "TEST SUCCESS"
+        printf "\033[32mTEST SUCCESS\033[0m\n"
     fi
     cat "${RESULTS}/${i}/report"
     printf "\n"
 done
 
 exit "$ERR"
+
