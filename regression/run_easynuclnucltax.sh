84740ba6d30cc49ee916f09e0a8134a1ad8bc137
Milot Mirdita <milot@mirdita.de>
2019-08-15 14:09:43 +0200

    Change nuclnucltax targets due to new spacing patterns

--- regression/run_easynuclnucltax.sh
+++ regression/run_easynuclnucltax.sh
@@ -36,7 +36,7 @@ R_BACTERIA=$(grep 'superkingdom.*Bacteria' "$RESULTS/results_aln_report" | cut -
 R_VIRUS=$(grep 'superkingdom.*Virus' "$RESULTS/results_aln_report" | cut -f 2)
 R_EUKARYOTA=$(grep 'superkingdom.*Eukaryota' "$RESULTS/results_aln_report" | cut -f 2)
 
-TARGET="from taxonomyreport: 2632 239 2595"
+TARGET="from taxonomyreport: 2587 238 2638"
 ACTUAL="from taxonomyreport: $R_BACTERIA $R_VIRUS $R_EUKARYOTA"
 awk -v actual="$ACTUAL" -v target="$TARGET" 'BEGIN { print (actual == target) ? "GOOD" : "BAD"; \
     print "Expected: ", target; \
