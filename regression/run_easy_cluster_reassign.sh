5e5f581703d2b1a003728ba75ac5eff3e8b20eba
Martin Steinegger <themartinsteinegger@gmail.com>
2019-12-03 08:01:01 -0500

    Update regression to text stdin

--- regression/run_easy_cluster_reassign.sh
+++ regression/run_easy_cluster_reassign.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -e
 
-"${MMSEQS}" easy-cluster "${DATADIR}/clu.fasta" "$RESULTS/results" "$RESULTS/tmp" --min-seq-id 0.3 -s 2 -c 0.8 --cov-mode 1 --cluster-reassign 1
+cat  "${DATADIR}/clu.fasta" | "${MMSEQS}" easy-cluster stdin "$RESULTS/results" "$RESULTS/tmp" --min-seq-id 0.3 -s 2 -c 0.8 --cov-mode 1 --cluster-reassign 1
 
 awk 'BEGIN { l = "" } l != $1 { l = $1; cnt++; } { t++; } END { print cnt"\t"t"\t"(t/cnt) }' "$RESULTS/results_cluster.tsv" > "$RESULTS/results_summary.tsv"
 ACTUAL="$(cut -f1 "$RESULTS/results_summary.tsv")"
--- regression/run_easy_search.sh
+++ regression/run_easy_search.sh
@@ -2,7 +2,7 @@
 QUERY="${DATADIR}/query.fasta"
 TARGET="${DATADIR}/targetannotation.fasta"
 
-"${MMSEQS}" easy-search "$QUERY" "$TARGET" "$RESULTS/results_aln.m8" "$RESULTS/tmp" -e 10000 -s 4 --max-seqs 4000 --compressed 1
+cat "$QUERY" | "${MMSEQS}" easy-search stdin "$TARGET" "$RESULTS/results_aln.m8" "$RESULTS/tmp" -e 10000 -s 4 --max-seqs 4000 --compressed 1
 
 "${EVALUATE}" "$QUERY" "$TARGET" "$RESULTS/results_aln.m8" "${RESULTS}/evaluation_roc5.dat" 4000 1 | tee "${RESULTS}/evaluation.log"
 ACTUAL=$(grep "^ROC5 AUC:" "${RESULTS}/evaluation.log" | cut -d" " -f3)
