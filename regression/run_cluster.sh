0eb09c3ff74d3bea51c506077d0fe4cd98c0cded
Martin Steinegger <themartinsteinegger@gmail.com>
2020-02-07 08:04:50 -0500

    Update regression test

--- regression/run_cluster.sh
+++ regression/run_cluster.sh
@@ -8,7 +8,7 @@ CLUDB="${RESULTS}/clu"
 
 awk 'BEGIN { l = "" } l != $1 { l = $1; cnt++; } { t++; } END { print cnt"\t"t"\t"(t/cnt) }' "$RESULTS/results_cluster.tsv" > "$RESULTS/results_summary.tsv"
 ACTUAL="$(cut -f1 "$RESULTS/results_summary.tsv")"
-TARGET="15721"
+TARGET="15722"
 awk -v actual="$ACTUAL" -v target="$TARGET" \
     'BEGIN { print (actual == target) ? "GOOD" : "BAD"; print "Expected: ", target; print "Actual: ", actual; }' \
     > "${RESULTS}.report"
