ebfd230ec72aef853e001e6ab878730648e0b97c
Milot Mirdita <milot@mirdita.de>
2020-02-23 12:47:41 +0100

    Add aarch64 samtools

--- samtools/samtools.sh
+++ samtools/samtools.sh
@@ -1,7 +1,15 @@
 #!/bin/bash -e
 SELF="$( cd "$(dirname "$0")" ; pwd -P )"
+SUFFIX=""
+case "$(uname -m)" in
+  arm*|aarch*)
+   SUFFIX="-aarch64"
+   # upx is not working with samtools on aarch64
+   gunzip "$SELF/samtools-linux${SUFFIX}.gz"
+   ;;
+esac
 case "$(echo "$OSTYPE" | tr '[:upper:]' '[:lower:]')" in
-  linux*) "$SELF/samtools-linux" "$@" ;;
+  linux*) "$SELF/samtools-linux$SUFFIX" "$@" ;;
   darwin*) "$SELF/samtools-darwin" "$@" ;;
   msys*|cygwin*) "$SELF/samtools-windows" "$@" ;;
   *) exit 1 ;;
