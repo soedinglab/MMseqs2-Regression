64cc3bf0430f46a2f13f13295317cd55a933c84c
Milot Mirdita <milot@mirdita.de>
2020-06-21 01:06:36 +0200

    Fix profile AUC value for account for higher precision SIMD division

--- regression/run_profile.sh
+++ regression/run_profile.sh
@@ -14,7 +14,7 @@ LC_ALL=C sort -k1,1 -k11,11g "$RESULTS/results_aln.m8" > "$RESULTS/results_aln_s
 
 "${EVALUATE}" "$QUERY" "$TARGET" "$RESULTS/results_aln_sorted.m8" "${RESULTS}/evaluation_roc5.dat" 4000 1 | tee "${RESULTS}/evaluation.log"
 ACTUAL=$(grep "^ROC5 AUC:" "${RESULTS}/evaluation.log" | cut -d" " -f3)
-TARGET="0.367398"
+TARGET="0.367396"
 awk -v actual="$ACTUAL" -v target="$TARGET" \
     'BEGIN { print (actual >= target) ? "GOOD" : "BAD"; print "Expected: ", target; print "Actual: ", actual; }' \
     > "${RESULTS}.report"
