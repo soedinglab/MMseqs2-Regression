e26f1176c67122a11d53cdc882bb6592703f0bb6
Milot Mirdita <milot@mirdita.de>
2020-08-10 17:16:25 +0200

    Add result2pp test

--- regression/run_expand.sh
+++ regression/run_expand.sh
@@ -1,10 +1,10 @@
 #!/bin/sh -e
 QUERY="${DATADIR}/query.fasta"
 QUERYDB="${RESULTS}/query"
-"${MMSEQS}" createdb "${QUERY}" "${QUERYDB}"
 
 TARGET="${DATADIR}/targetannotation.fasta"
 TARGETDB="${RESULTS}/targetannotation"
+"${MMSEQS}" createdb "${QUERY}" "${QUERYDB}"
 "${MMSEQS}" createdb "${TARGET}" "${TARGETDB}"
 "${MMSEQS}" search "$TARGETDB" "$TARGETDB" "$RESULTS/results_bc" "$RESULTS/tmp" -s 1 -a
 
@@ -22,6 +22,15 @@ TARGET1="0.16614"
 ACTUAL2=$(grep "^ROC5 AUC:" "${RESULTS}/evaluation.log" | cut -d" " -f3)
 TARGET2="0.171723"
 
-awk -v actual1="$ACTUAL1" -v target1="$TARGET1" -v actual2="$ACTUAL2" -v target2="$TARGET2" \
-    'BEGIN { print (actual1 >= target1 && actual2 >= target2) ? "GOOD" : "BAD"; print "Expected: "target1", "target2; print "Actual: "actual1", "actual2; }' \
+"${MMSEQS}" result2profile "$TARGETDB" "$TARGETDB" "$RESULTS/results_bc" "$RESULTS/prof_bc"
+"${MMSEQS}" result2profile "$QUERYDB" "$TARGETDB" "$RESULTS/results_ab" "$RESULTS/prof_ab"
+"${MMSEQS}" result2pp "$RESULTS/prof_ab" "$RESULTS/prof_bc" "$RESULTS/results_ab" "$RESULTS/prof_merged"
+"${MMSEQS}" search "$RESULTS/prof_merged" "$TARGETDB" "$RESULTS/results_merged" "$RESULTS/tmp" -e 10000 -s 2 -a --max-seqs 4000
+"${MMSEQS}" convertalis "$QUERYDB" "$TARGETDB" "$RESULTS/results_merged" "$RESULTS/results_merged.m8"
+"${EVALUATE}" "$QUERY" "$TARGET" "$RESULTS/results_merged.m8" "${RESULTS}/evaluation_roc5.dat" 4000 1 | tee "${RESULTS}/evaluation.log"
+ACTUAL3=$(grep "^ROC5 AUC:" "${RESULTS}/evaluation.log" | cut -d" " -f3)
+TARGET3="0.222102"
+
+awk -v actual1="$ACTUAL1" -v target1="$TARGET1" -v actual2="$ACTUAL2" -v target2="$TARGET2" -v actual3="$ACTUAL3" -v target3="$TARGET3" \
+    'BEGIN { print (actual1 >= target1 && actual2 >= target2 && actual3 >= target3) ? "GOOD" : "BAD"; print "Expected: "target1", "target2", "target3; print "Actual: "actual1", "actual2", "actual3; }' \
     > "${RESULTS}.report"
