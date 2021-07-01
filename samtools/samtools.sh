a2d56e8786ff754a8aec58090e291949b1c19163
Milot Mirdita <milot@mirdita.de>
2021-07-01 17:50:59 +0200

    Remove bash-ism from samtools.sh

--- samtools/samtools.sh
+++ samtools/samtools.sh
@@ -1,15 +1,14 @@
-#!/usr/bin/env bash
-set -e
+#!/bin/sh -e
 SELF="$( cd "$(dirname "$0")" ; pwd -P )"
 SUFFIX=""
 case "$(uname -m)" in
   arm*|aarch*) SUFFIX="-aarch64" ;;
   ppc*) SUFFIX="-ppc64le" ;;
 esac
-case "$(echo "$OSTYPE" | tr '[:upper:]' '[:lower:]')" in
+case "$(uname -s | tr '[:upper:]' '[:lower:]')" in
   linux*) exec "$SELF/samtools-linux$SUFFIX" "$@" ;;
   darwin*) exec "$SELF/samtools-darwin" "$@" ;;
-  msys*|cygwin*) exec "$SELF/samtools-windows" "$@" ;;
+  msys*|cygwin*|mingw*) exec "$SELF/samtools-windows" "$@" ;;
 esac
 echo "No precompiled samtools found, using system samtools" >&2
 samtools "$@"
