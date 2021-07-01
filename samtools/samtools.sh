a0e096a9e905624cb3121d2f8969844eecde0eb0
Milot Mirdita <milot@mirdita.de>
2021-07-01 16:51:50 +0200

    Allow using system samtools if no precompiled samtools is available

--- samtools/samtools.sh
+++ samtools/samtools.sh
@@ -1,4 +1,5 @@
-#!/bin/bash -e
+#!/usr/bin/env bash
+set -e
 SELF="$( cd "$(dirname "$0")" ; pwd -P )"
 SUFFIX=""
 case "$(uname -m)" in
@@ -6,8 +7,9 @@ case "$(uname -m)" in
   ppc*) SUFFIX="-ppc64le" ;;
 esac
 case "$(echo "$OSTYPE" | tr '[:upper:]' '[:lower:]')" in
-  linux*) "$SELF/samtools-linux$SUFFIX" "$@" ;;
-  darwin*) "$SELF/samtools-darwin" "$@" ;;
-  msys*|cygwin*) "$SELF/samtools-windows" "$@" ;;
-  *) exit 1 ;;
+  linux*) exec "$SELF/samtools-linux$SUFFIX" "$@" ;;
+  darwin*) exec "$SELF/samtools-darwin" "$@" ;;
+  msys*|cygwin*) exec "$SELF/samtools-windows" "$@" ;;
 esac
+echo "No precompiled samtools found, using system samtools" >&2
+samtools "$@"
