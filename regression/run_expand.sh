25aa30611c4786a869dffed3bf1883867637aa4b
Milot Mirdita <milot@mirdita.de>
2021-08-08 21:46:02 +0200

    Remove result2pp calls from expand regression

--- regression/run_expand.sh
+++ regression/run_expand.sh
@@ -22,15 +22,6 @@ TARGET1="0.16614"
 ACTUAL2=$(grep "^ROC5 AUC:" "${RESULTS}/evaluation.log" | cut -d" " -f3)
 TARGET2="0.171723"
 
-"${MMSEQS}" result2profile "$TARGETDB" "$TARGETDB" "$RESULTS/results_bc" "$RESULTS/prof_bc"
-"${MMSEQS}" result2profile "$QUERYDB" "$TARGETDB" "$RESULTS/results_ab" "$RESULTS/prof_ab"
-"${MMSEQS}" result2pp "$RESULTS/prof_ab" "$RESULTS/prof_bc" "$RESULTS/results_ab" "$RESULTS/prof_merged"
-"${MMSEQS}" search "$RESULTS/prof_merged" "$TARGETDB" "$RESULTS/results_merged" "$RESULTS/tmp" -e 10000 -s 2 -a --max-seqs 4000
-"${MMSEQS}" convertalis "$QUERYDB" "$TARGETDB" "$RESULTS/results_merged" "$RESULTS/results_merged.m8"
-"${EVALUATE}" "$QUERY" "$TARGET" "$RESULTS/results_merged.m8" "${RESULTS}/evaluation_roc5.dat" 4000 1 | tee "${RESULTS}/evaluation.log"
-ACTUAL3=$(grep "^ROC5 AUC:" "${RESULTS}/evaluation.log" | cut -d" " -f3)
-TARGET3="0.222982"
-
-awk -v actual1="$ACTUAL1" -v target1="$TARGET1" -v actual2="$ACTUAL2" -v target2="$TARGET2" -v actual3="$ACTUAL3" -v target3="$TARGET3" \
-    'BEGIN { print (actual1 >= target1 && actual2 >= target2 && actual3 >= target3) ? "GOOD" : "BAD"; print "Expected: "target1", "target2", "target3; print "Actual: "actual1", "actual2", "actual3; }' \
+awk -v actual1="$ACTUAL1" -v target1="$TARGET1" -v actual2="$ACTUAL2" -v target2="$TARGET2" \
+    'BEGIN { print (actual1 >= target1 && actual2 >= target2) ? "GOOD" : "BAD"; print "Expected: "target1", "target2; print "Actual: "actual1", "actual2; }' \
     > "${RESULTS}.report"
