82895cb98e7515685592fb9b439038c036eb39f2
Milot Mirdita <milot@mirdita.de>
2021-09-24 15:51:13 +0200

    Add initial version of sensitivity fitting script

--- regression/run_sliceprofile.sh
+++ regression/run_sliceprofile.sh
@@ -11,7 +11,8 @@ TARGETDB="${RESULTS}/targetannotation"
 awk 'BEGIN { printf("%c%c%c%c",11,0,0,0); exit; }' > "${TARGETDB}_fasta.dbtype"
 "${MMSEQS}" msa2profile "${TARGETDB}_fasta" "${TARGETDB}_profile" --filter-msa 0
 
-"${MMSEQS}" search "$QUERYDB" "${TARGETDB}_profile" "$RESULTS/results_aln" "$RESULTS/tmp" -s 1 --exhaustive-search -e 10000
+SENS_PARAM=${1:-"-s 1"}
+"${MMSEQS}" search "$QUERYDB" "${TARGETDB}_profile" "$RESULTS/results_aln" "$RESULTS/tmp" --exhaustive-search -e 10000 ${SENS_PARAM}
 "${MMSEQS}" convertalis "$QUERYDB" "${TARGETDB}_profile" "$RESULTS/results_aln" "$RESULTS/results_aln.m8"
 
 "${EVALUATE}" "$QUERY" "$TARGET" "$RESULTS/results_aln.m8" "${RESULTS}/evaluation_roc5.dat" 4000 1 | tee "${RESULTS}/evaluation.log"
