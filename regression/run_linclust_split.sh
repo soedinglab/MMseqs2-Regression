6a2df6bd0d54975813cf2f1424e296a55271cfcc
Joey Lee <leejoey0921@gmail.com>
2024-09-02 13:00:17 +0900

    feat: update regression results

--- regression/run_linclust_split.sh
+++ regression/run_linclust_split.sh
@@ -6,7 +6,7 @@
 
 awk 'BEGIN { l = "" } l != $1 { l = $1; cnt++; } { t++; } END { print cnt"\t"t"\t"(t/cnt) }' "$RESULTS/results_cluster.tsv" > "$RESULTS/results_summary.tsv"
 ACTUAL="$(cut -f1 "$RESULTS/results_summary.tsv")"
-TARGET="26491"
+TARGET="26135"
 awk -v actual="$ACTUAL" -v target="$TARGET" \
     'BEGIN { print (actual == target) ? "GOOD" : "BAD"; print "Expected: ", target; print "Actual: ", actual; }' \
     > "${RESULTS}.report"
